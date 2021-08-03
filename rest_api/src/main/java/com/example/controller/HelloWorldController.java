8c53a073c52139533a97d8e8c5c37f00d727405c
서민영 <tjalsdud9@gmail.com>
2021-08-03 14:07:57 +0900

    update crossorigin

--- rest_api/src/main/java/com/example/controller/HelloWorldController.java
+++ rest_api/src/main/java/com/example/controller/HelloWorldController.java
@@ -1,9 +1,11 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class HelloWorldController {
 	private static final String template = "Hello, %s!";
